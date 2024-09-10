f0b14b772f73ce3f07f4eb54576dc5b1747010c7
Vincent Tai <177089640+Liyang-86@users.noreply.github.com>
2024-09-10 21:11:29 +0800

    prototype 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const arr = new Array();
+    for (let i = 0; i < string.length; i++) {
+        arr[string.length - i] = string[i];
+        reverseArr = arr.join();
+    };
+    console.log(reverseArr);
+    return reverseArr;
 };
 
 // Do not edit below this line
