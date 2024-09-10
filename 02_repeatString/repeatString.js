e5e3145f0b3c0dbbd4c8948831f8934cfc2675c1
Vincent Tai <177089640+Liyang-86@users.noreply.github.com>
2024-09-10 20:01:39 +0800

    finish 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 1 && num >= 0) return '';
+    if (num < 0) return 'ERROR';
+    let str = string;
+    for (let i = 1; i < num; i++) {
+        string += str;
+    };
+    return string;
 };
 
 // Do not edit below this line
