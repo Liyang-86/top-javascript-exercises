cc9f037381124e3186b660649a7177dce525c892
Vincent Tai <177089640+Liyang-86@users.noreply.github.com>
2024-09-11 14:18:12 +0800

    solve 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,11 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if (num1 > 0 && num2 > 0 && Number.isInteger(num1) && Number.isInteger(num2)) {
+        largeNum = Math.max(num1, num2);
+        smallNum = Math.min(num1, num2);
+        return sum = (largeNum - smallNum + 1) * (largeNum + smallNum) / 2;
+    } else {
+        return 'ERROR';
+    }
 };
 
 // Do not edit below this line
