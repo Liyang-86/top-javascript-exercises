d15a83eb31c04221be07ea57e14d29a96882af6f
Vincent Tai <177089640+Liyang-86@users.noreply.github.com>
2024-09-11 13:51:29 +0800

    solve 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...nums) {
+    for (const i of nums) {
+        arr = arr.filter((x) => {
+            return x !== i;
+        });
+    }
+    return arr;
 };
 
 // Do not edit below this line
