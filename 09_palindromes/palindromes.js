bd42b3658ce814136d586cc4a72cd75e920e3bc0
Vincent Tai <177089640+Liyang-86@users.noreply.github.com>
2024-09-19 14:55:31 +0800

    solve 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str.replace(/[^\w\']/g, "").toLowerCase().split('');
+    console.log(str);
+    for (let i = 0; i <= str.length - i; i++) {
+        if (str[i] != str[str.length - i - 1]) return false;
+    }
+    return true;
 };
 
 // Do not edit below this line
