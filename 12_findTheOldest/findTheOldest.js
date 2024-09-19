3645b0196865e82c853ea71ba038ad2c7a091555
Vincent Tai <177089640+Liyang-86@users.noreply.github.com>
2024-09-19 16:33:43 +0800

    solve 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    people.sort((a, b) => {
+        if (a.yearOfDeath === undefined) a.yearOfDeath = new Date().getFullYear();
+        if (b.yearOfDeath === undefined) b.yearOfDeath = new Date().getFullYear();
+        return (a.yearOfDeath - a.yearOfBirth) - (b.yearOfDeath - b.yearOfBirth);
+    });
+    console.log(people);
+    return people[people.length - 1];
 };
 
 // Do not edit below this line
