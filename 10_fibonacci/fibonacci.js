e119f90f286d642ae6778bc20c56a255496aa36a
Vincent Tai <177089640+Liyang-86@users.noreply.github.com>
2024-09-19 15:43:56 +0800

    solve 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,8 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+    if (index < 0) return "OOPS";
+    if (index == 0) return 0;
+    if (index == 1) return 1;
+    return fibonacci(index - 1) + fibonacci(index - 2);
 };
 
 // Do not edit below this line
